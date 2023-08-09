909edd7c8dee0d224cfab539288f02f191c16f3e
Tomas Jaros <tjaros.822@gmail.com>
2023-08-09 21:53:37 +0200

    feat: steal waybar config

--- modules/waybar/style.nix
+++ modules/waybar/style.nix
@@ -1,141 +1,426 @@
+{ outputs, config, lib, pkgs, ... }:
+
 let
-  snowflake = builtins.fetchurl rec {
-    name = "Logo-${sha256}.svg";
-    url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/master/logo/nix-snowflake.svg";
-    sha256 = "14mbpw8jv1w2c5wvfvj8clmjw0fi956bq5xf9s2q3my14far0as8";
+  # Dependencies
+  cat = "${pkgs.coreutils}/bin/cat";
+  cut = "${pkgs.coreutils}/bin/cut";
+  find = "${pkgs.findutils}/bin/find";
+  grep = "${pkgs.gnugrep}/bin/grep";
+  perl = "${pkgs.perl}/bin/perl";
+  pgrep = "${pkgs.procps}/bin/pgrep";
+  sed = "${pkgs.gnused}/bin/sed";
+  tail = "${pkgs.coreutils}/bin/tail";
+  wc = "${pkgs.coreutils}/bin/wc";
+  xargs = "${pkgs.findutils}/bin/xargs";
+  timeout = "${pkgs.coreutils}/bin/timeout";
+  ping = "${pkgs.iputils}/bin/ping";
+
+  jq = "${pkgs.jq}/bin/jq";
+  xml = "${pkgs.xmlstarlet}/bin/xml";
+  gamemoded = "${pkgs.gamemode}/bin/gamemoded";
+  systemctl = "${pkgs.systemd}/bin/systemctl";
+  journalctl = "${pkgs.systemd}/bin/journalctl";
+  playerctl = "${pkgs.playerctl}/bin/playerctl";
+  playerctld = "${pkgs.playerctl}/bin/playerctld";
+  pavucontrol = "${pkgs.pavucontrol}/bin/pavucontrol";
+  wofi = "${pkgs.wofi}/bin/wofi";
+
+  # Function to simplify making waybar outputs
+  jsonOutput = name: { pre ? "", text ? "", tooltip ? "", alt ? "", class ? "", percentage ? "" }: "${pkgs.writeShellScriptBin "waybar-${name}" ''
+    set -euo pipefail
+    ${pre}
+    ${jq} -cn \
+      --arg text "${text}" \
+      --arg tooltip "${tooltip}" \
+      --arg alt "${alt}" \
+      --arg class "${class}" \
+      --arg percentage "${percentage}" \
+      '{text:$text,tooltip:$tooltip,alt:$alt,class:$class,percentage:$percentage}'
+  ''}/bin/waybar-${name}";
+in
+{
+  programs.waybar = {
+    enable = true;
+    package = pkgs.waybar.overrideAttrs (oa: {
+      mesonFlags = (oa.mesonFlags or  [ ]) ++ [ "-Dexperimental=true" ];
+    });
+    systemd.enable = true;
+    settings = {
+
+      secondary = {
+        mode = "dock";
+        layer = "top";
+        height = 32;
+        width = 100;
+        margin = "6";
+        position = "bottom";
+        modules-center = (lib.optionals config.wayland.windowManager.sway.enable [
+          "sway/workspaces"
+          "sway/mode"
+        ]) ++ (lib.optionals config.wayland.windowManager.hyprland.enable [
+          "wlr/workspaces"
+        ]);
+
+        "wlr/workspaces" = {
+          on-click = "activate";
+        };
+      };
+
+      primary = {
+        mode = "dock";
+        layer = "top";
+        height = 40;
+        margin = "6";
+        position = "top";
+        output = builtins.map (m: m.name) (builtins.filter (m: ! m.noBar) config.monitors);
+        modules-left = [
+          "custom/menu"
+          "custom/currentplayer"
+          "custom/player"
+        ];
+        modules-center = [
+          "cpu"
+          "custom/gpu"
+          "memory"
+          "clock"
+          "pulseaudio"
+          "custom/unread-mail"
+          "custom/gammastep"
+          "custom/gpg-agent"
+        ];
+        modules-right = [
+          "custom/gamemode"
+          "network"
+          "custom/tailscale-ping"
+          "battery"
+          "tray"
+          "custom/hostname"
+        ];
+
+        clock = {
+          format = "{:%d/%m %H:%M}";
+          tooltip-format = ''
+            <big>{:%Y %B}</big>
+            <tt><small>{calendar}</small></tt>'';
+        };
+        cpu = {
+          format = "   {usage}%";
+        };
+        "custom/gpu" = {
+          interval = 5;
+          return-type = "json";
+          exec = jsonOutput "gpu" {
+            text = "$(${cat} /sys/class/drm/card0/device/gpu_busy_percent)";
+            tooltip = "GPU Usage";
+          };
+          format = "󰒋  {}%";
+        };
+        memory = {
+          format = "󰍛  {}%";
+          interval = 5;
+        };
+        pulseaudio = {
+          format = "{icon}  {volume}%";
+          format-muted = "   0%";
+          format-icons = {
+            headphone = "󰋋";
+            headset = "󰋎";
+            portable = "";
+            default = [ "" "" "" ];
+          };
+          on-click = pavucontrol;
+        };
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+            activated = "󰒳";
+            deactivated = "󰒲";
+          };
+        };
+        battery = {
+          bat = "BAT0";
+          interval = 10;
+          format-icons = [ "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
+          format = "{icon} {capacity}%";
+          format-charging = "󰂄 {capacity}%";
+          onclick = "";
+        };
+        "sway/window" = {
+          max-length = 20;
+        };
+        network = {
+          interval = 3;
+          format-wifi = "   {essid}";
+          format-ethernet = "󰈁 Connected";
+          format-disconnected = "";
+          tooltip-format = ''
+            {ifname}
+            {ipaddr}/{cidr}
+            Up: {bandwidthUpBits}
+            Down: {bandwidthDownBits}'';
+          on-click = "";
+        };
+        "custom/tailscale-ping" = {
+          interval = 2;
+          return-type = "json";
+          exec =
+            let
+              inherit (builtins) concatStringsSep attrNames;
+              hosts = attrNames outputs.nixosConfigurations;
+              homeMachine = "merope";
+              remoteMachine = "alcyone";
+            in
+            jsonOutput "tailscale-ping" {
+              # Build variables for each host
+              pre = ''
+                set -o pipefail
+                ${concatStringsSep "\n" (map (host: ''
+                  ping_${host}="$(${timeout} 2 ${ping} -c 1 -q ${host} 2>/dev/null | ${tail} -1 | ${cut} -d '/' -f5 | ${cut} -d '.' -f1)ms" || ping_${host}="Disconnected"
+                '') hosts)}
+              '';
+              # Access a remote machine's and a home machine's ping
+              text = "  $ping_${remoteMachine} /  $ping_${homeMachine}";
+              # Show pings from all machines
+              tooltip = concatStringsSep "\n" (map (host: "${host}: $ping_${host}") hosts);
+            };
+          format = "{}";
+          on-click = "";
+        };
+        "custom/menu" = {
+          return-type = "json";
+          exec = jsonOutput "menu" {
+            text = "";
+            tooltip = ''$(${cat} /etc/os-release | ${grep} PRETTY_NAME | ${cut} -d '"' -f2)'';
+          };
+          on-click = "${wofi} -S drun -x 10 -y 10 -W 25% -H 60%";
+        };
+        "custom/hostname" = {
+          exec = "echo $USER@$HOSTNAME";
+        };
+        "custom/unread-mail" = {
+          interval = 5;
+          return-type = "json";
+          exec = jsonOutput "unread-mail" {
+            pre = ''
+              count=$(${find} ~/Mail/*/Inbox/new -type f | ${wc} -l)
+              if [ "$count" == "0" ]; then
+                subjects="No new mail"
+                status="read"
+              else
+                subjects=$(\
+                  ${grep} -h "Subject: " -r ~/Mail/*/Inbox/new | ${cut} -d ':' -f2- | \
+                  ${perl} -CS -MEncode -ne 'print decode("MIME-Header", $_)' | ${xml} esc | ${sed} -e 's/^/\-/'\
+                )
+                status="unread"
+              fi
+              if ${pgrep} mbsync &>/dev/null; then
+                status="syncing"
+              fi
+            '';
+            text = "$count";
+            tooltip = "$subjects";
+            alt = "$status";
+          };
+          format = "{icon}  ({})";
+          format-icons = {
+            "read" = "󰇯";
+            "unread" = "󰇮";
+            "syncing" = "󰁪";
+          };
+        };
+        "custom/gpg-agent" = {
+          interval = 2;
+          return-type = "json";
+          exec =
+            let gpgCmds = import ../../../cli/gpg-commands.nix { inherit pkgs; };
+            in
+            jsonOutput "gpg-agent" {
+              pre = ''status=$(${gpgCmds.isUnlocked} && echo "unlocked" || echo "locked")'';
+              alt = "$status";
+              tooltip = "GPG is $status";
+            };
+          format = "{icon}";
+          format-icons = {
+            "locked" = "";
+            "unlocked" = "";
+          };
+          on-click = "";
+        };
+        "custom/gamemode" = {
+          exec-if = "${gamemoded} --status | ${grep} 'is active' -q";
+          interval = 2;
+          return-type = "json";
+          exec = jsonOutput "gamemode" {
+            tooltip = "Gamemode is active";
+          };
+          format = " ";
+        };
+        "custom/gammastep" = {
+          interval = 5;
+          return-type = "json";
+          exec = jsonOutput "gammastep" {
+            pre = ''
+              if unit_status="$(${systemctl} --user is-active gammastep)"; then
+                status="$unit_status ($(${journalctl} --user -u gammastep.service -g 'Period: ' | ${tail} -1 | ${cut} -d ':' -f6 | ${xargs}))"
+              else
+                status="$unit_status"
+              fi
+            '';
+            alt = "\${status:-inactive}";
+            tooltip = "Gammastep is $status";
+          };
+          format = "{icon}";
+          format-icons = {
+            "activating" = "󰁪 ";
+            "deactivating" = "󰁪 ";
+            "inactive" = "? ";
+            "active (Night)" = " ";
+            "active (Nighttime)" = " ";
+            "active (Transition (Night)" = " ";
+            "active (Transition (Nighttime)" = " ";
+            "active (Day)" = " ";
+            "active (Daytime)" = " ";
+            "active (Transition (Day)" = " ";
+            "active (Transition (Daytime)" = " ";
+          };
+          on-click = "${systemctl} --user is-active gammastep && ${systemctl} --user stop gammastep || ${systemctl} --user start gammastep";
+        };
+        "custom/currentplayer" = {
+          interval = 2;
+          return-type = "json";
+          exec = jsonOutput "currentplayer" {
+            pre = ''
+              player="$(${playerctl} status -f "{{playerName}}" 2>/dev/null || echo "No player active" | ${cut} -d '.' -f1)"
+              count="$(${playerctl} -l | ${wc} -l)"
+              if ((count > 1)); then
+                more=" +$((count - 1))"
+              else
+                more=""
+              fi
+            '';
+            alt = "$player";
+            tooltip = "$player ($count available)";
+            text = "$more";
+          };
+          format = "{icon}{}";
+          format-icons = {
+            "No player active" = " ";
+            "Celluloid" = "󰎁 ";
+            "spotify" = " 󰓇";
+            "ncspot" = " 󰓇";
+            "qutebrowser" = "󰖟";
+            "firefox" = " ";
+            "discord" = " 󰙯 ";
+            "sublimemusic" = " ";
+            "kdeconnect" = "󰄡 ";
+          };
+          on-click = "${playerctld} shift";
+          on-click-right = "${playerctld} unshift";
+        };
+        "custom/player" = {
+          exec-if = "${playerctl} status";
+          exec = ''${playerctl} metadata --format '{"text": "{{artist}} - {{title}}", "alt": "{{status}}", "tooltip": "{{title}} ({{artist}} - {{album}})"}' '';
+          return-type = "json";
+          interval = 2;
+          max-length = 30;
+          format = "{icon} {}";
+          format-icons = {
+            "Playing" = "󰐊";
+            "Paused" = "󰏤 ";
+            "Stopped" = "󰓛";
+          };
+          on-click = "${playerctl} play-pause";
+        };
+      };
+
+    };
+    # Cheatsheet:
+    # x -> all sides
+    # x y -> vertical, horizontal
+    # x y z -> top, horizontal, bottom
+    # w x y z -> top, right, bottom, left
+    style = let inherit (config.colorscheme) colors; in /* css */ ''
+      * {
+        font-family: ${config.fontProfiles.regular.family}, ${config.fontProfiles.monospace.family};
+        font-size: 12pt;
+        padding: 0 8px;
+      }
+
+      .modules-right {
+        margin-right: -15px;
+      }
+
+      .modules-left {
+        margin-left: -15px;
+      }
+
+      window#waybar.top {
+        opacity: 0.95;
+        padding: 0;
+        background-color: #${colors.base00};
+        border: 2px solid #${colors.base0C};
+        border-radius: 10px;
+      }
+      window#waybar.bottom {
+        opacity: 0.90;
+        background-color: #${colors.base00};
+        border: 2px solid #${colors.base0C};
+        border-radius: 10px;
+      }
+
+      window#waybar {
+        color: #${colors.base05};
+      }
+
+      #workspaces button {
+        background-color: #${colors.base01};
+        color: #${colors.base05};
+        margin: 4px;
+      }
+      #workspaces button.hidden {
+        background-color: #${colors.base00};
+        color: #${colors.base04};
+      }
+      #workspaces button.focused,
+      #workspaces button.active {
+        background-color: #${colors.base0A};
+        color: #${colors.base00};
+      }
+
+      #clock {
+        background-color: #${colors.base0C};
+        color: #${colors.base00};
+        padding-left: 15px;
+        padding-right: 15px;
+        margin-top: 0;
+        margin-bottom: 0;
+        border-radius: 10px;
+      }
+
+      #custom-menu {
+        background-color: #${colors.base0C};
+        color: #${colors.base00};
+        padding-left: 15px;
+        padding-right: 22px;
+        margin-left: 0;
+        margin-right: 10px;
+        margin-top: 0;
+        margin-bottom: 0;
+        border-radius: 10px;
+      }
+      #custom-hostname {
+        background-color: #${colors.base0C};
+        color: #${colors.base00};
+        padding-left: 15px;
+        padding-right: 18px;
+        margin-right: 0;
+        margin-top: 0;
+        margin-bottom: 0;
+        border-radius: 10px;
+      }
+      #tray {
+        color: #${colors.base05};
+      }
+    '';
   };
-in ''
-  * {
-    /* `otf-font-awesome` is required to be installed for icons */
-    font-family: Material Design Icons, Iosevka Nerd Font;
-  }
-
-  window#waybar {
-    background-color: rgba(30, 30, 46, 0.7);
-    border-radius: 0px;
-    color: #cdd6f4;
-    font-size: 16px;
-    /* transition-property: background-color; */
-    transition-duration: 0.5s;
-  }
-
-  window#waybar.hidden {
-    opacity: 0.2;
-  }
-
-  #pulseaudio {
-    color: #a6d189;
-  }
-
-  #custom-vpn,
-  #network {
-    color: #ca9ee6;
-  }
-
-  #cpu {
-    color: #ef9f76;
-  }
-
-  #workspaces button {
-    background-color: transparent;
-    /* Use box-shadow instead of border so the text isn't offset */
-    color: #89b4fa;
-    padding-left: 6px;
-    box-shadow: inset 0 -3px transparent;
-    transition: all 0.5s cubic-bezier(.55,-0.68,.48,1.68);
-  }
-
-  #workspaces button:hover {
-    color: #b4befe;
-    box-shadow: inherit;
-    text-shadow: inherit;
-  }
-
-  #custom-power {
-      color: #f38ba8;
-      margin: 7px;
-      font-size: 24px;
-  }
-
-  #workspaces button.active {
-    color: #f9e2af;
-    transition: all 0.3s cubic-bezier(.55,-0.68,.48,1.682);
-  }
-
-  #workspaces button.urgent {
-    background-color: #eba0ac;
-  }
-  #custom-weather,
-
-  #clock,
-  #network,
-  #custom-swallow,
-  #cpu,
-  #battery,
-  #backlight,
-  #memory,
-  #workspaces,
-  #custom-todo,
-  #custom-lock,
-  #custom-vpn,
-  #custom-weather,
-  #custom-eth,
-  #volume,
-  #pulseaudio {
-    border-radius: 15px;
-    background-color: rgba(49, 50, 68, 0.8);
-    padding: 0px 10px 0px 10px;
-    margin: 5px 0px 5px 0px;
-  }
-
-  #custom-swallow {
-    color: #94e2d5;
-  }
-
-
-  #custom-eth {
-    color: #89b4fa;
-  }
-
-  #custom-lock {
-      color: #89b4fa;
-      padding: 0 20px 0 20px;
-  }
-
-  #custom-todo {
-    color: #bac2de;
-  }
-
-  #custom-search {
-    background-image: url("${snowflake}");
-    background-size: 65%;
-    padding: 0 15px 0 15px;
-    margin: 5px;
-    background-position: center;
-    background-repeat: no-repeat;
-  }
-  #backlight {
-    color: #f9e2af;
-  }
-  #battery {
-    color: #a6e3a1;
-  }
-
-  #battery.warning {
-    color: #fab387;
-  }
-
-  #battery.critical:not(.charging) {
-    color: #f38ba8;
-  }
-  tooltip {
-    font-family: 'Lato', sans-serif;
-    border-radius: 15px;
-    padding: 20px;
-    margin: 30px;
-  }
-  tooltip label {
-    font-family: 'Lato', sans-serif;
-    padding: 20px;
-  }
-''
+}
