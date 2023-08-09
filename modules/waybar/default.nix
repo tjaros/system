40289d241fa647aa55d79027fbd2e3653166e6cf
Tomas Jaros <tjaros.822@gmail.com>
2023-08-09 21:00:15 +0200

    fix: remove not working entries

--- modules/waybar/default.nix
+++ modules/waybar/default.nix
@@ -1,38 +1,6 @@
+ { pkgs, lib, config, ... }:
+
  {
-  pkgs,
-  lib,
-  config,
-  ...
-}: let
-  waybar-wttr = pkgs.stdenv.mkDerivation {
-    name = "waybar-wttr";
-    buildInputs = [
-      (pkgs.python39.withPackages
-        (pythonPackages: with pythonPackages; [requests]))
-    ];
-    unpackPhase = "true";
-    installPhase = ''
-      mkdir -p $out/bin
-      cp ${./waybar-wttr.py} $out/bin/waybar-wttr
-      chmod +x $out/bin/waybar-wttr
-    '';
-  };
-
-  get-crypto-price =
-    pkgs.writeShellScriptBin "get-crypto-price"
-    ''
-      #!/bin/sh
-      price="$(curl -s https://api.binance.com/api/v3/ticker/price?symbol=$1 | ${lib.getExe pkgs.jq} '.price' | sed 's/\"//g')"
-      printf "%.2f" $price
-    '';
-
-  mullvad-status =
-    pkgs.writeShellScriptBin "mullvad-status"
-    ''
-      #!/bin/sh
-      mullvad status | awk '{print $1;}'
-    '';
-in {
   xdg.configFile."waybar/style.css".text = import ./style.nix;
   programs.waybar = {
     enable = true;
@@ -52,18 +20,14 @@ in {
         fixed-center = false;
         exclusive = true;
         modules-left = [
-          "custom/search"
           "wlr/workspaces"
-          "custom/lock"
           "backlight"
           "battery"
-          "custom/eth"
         ];
         modules-center = [
-          "custom/weather"
           "clock"
         ];
-        modules-right = ["pulseaudio" "cpu" "network" "custom/power"];
+        modules-right = ["pulseaudio" "cpu" "network"];
         "wlr/workspaces" = {
           on-click = "activate";
           format = "{icon}";
@@ -72,60 +36,6 @@ in {
             default = "󰊠";
             active = " 󰮯";
           };
-        };
-        "custom/search" = {
-          format = " ";
-          tooltip = false;
-          on-click = lib.getExe config.programs.anyrun.package;
-        };
-
-        "custom/weather" = {
-          format = "{}";
-          tooltip = true;
-          interval = 30;
-          exec = "${waybar-wttr}/bin/waybar-wttr";
-          return-type = "json";
-        };
-        "custom/eth" = {
-          format = "󰡪 {}";
-          tooltip = true;
-          interval = 15;
-          exec = "${lib.getExe get-crypto-price} ETHUSDT";
-        };
-        "custom/vpn" = {
-          format = " VPN {}";
-          tooltip = true;
-          interval = 1;
-          exec = lib.getExe mullvad-status;
-        };
-        "custom/lock" = {
-          tooltip = false;
-          on-click = "sh -c '(sleep 0.5s; ${config.programs.swaylock.package}/bin/swaylock --grace 0)' & disown";
-          format = "";
-        };
-        "custom/swallow" = {
-          tooltip = false;
-          on-click = let
-            hyprctl = config.wayland.windowManager.hyprland.package + "/bin/hyprctl";
-            notify-send = pkgs.libnotify + "/bin/notify-send";
-            rg = pkgs.ripgrep + "/bin/rg";
-          in
-            pkgs.writeShellScript "waybar-swallow" ''
-              #!/bin/sh
-              if ${hyprctl} getoption misc:enable_swallow | ${rg}/bin/rg -q "int: 1"; then
-              	${hyprctl} keyword misc:enable_swallow false >/dev/null &&
-              		${notify-send} "Hyprland" "Turned off swallowing"
-              else
-              	${hyprctl} keyword misc:enable_swallow true >/dev/null &&
-              		${notify-send} "Hyprland" "Turned on swallowing"
-              fi
-            '';
-          format = "";
-        };
-        "custom/power" = {
-          tooltip = false;
-          # TODO
-          format = "󰐥 ";
         };
         clock = {
           format = "{:%b %d %H:%M}";
@@ -164,7 +74,7 @@ in {
           tooltip = false;
           format = "{icon} {volume}%";
           format-icons = {default = ["" "" "󰕾"];};
-          on-click = "${pkgs.killall}/bin/killall pavucontrol || ${pkgs.pavucontrol}/bin/pavucontrol";
+          on-click = "pkill pavucontrol || ${pkgs.pavucontrol}/bin/pavucontrol";
         };
       };
     };
