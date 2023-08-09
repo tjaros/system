8d88fcd7e8366b42095586ae5e06068a521112e6
Tomas Jaros <tjaros.822@gmail.com>
2023-08-09 23:28:13 +0200

    feat: add plasma xconfig

---
+++ nixos/global/plasma.nix
@@ -0,0 +1,24 @@
+
+{
+  services = {
+    xserver = {
+      enable = true;
+      videoDrivers = ["nvidia"];
+      displayManager = {
+        gdm = {
+          enable = true;
+          wayland = true;
+        };
+      };
+      layout = "us";
+      xkbVariant = "dvorak";
+    };
+  };
+
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+}
