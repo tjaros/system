bd988ff3cf899923a3bafe12de5e8325f315a959
Tomas Jaros <tjaros.822@gmail.com>
2023-08-09 16:54:49 +0200

    feat: split software configs into modules

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,14 +1,8 @@
-# This is your home-manager configuration file
-# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{ inputs, lib, config, pkgs, ... }:
 
-{ inputs, lib, config, pkgs, ... }: {
-  # You can import other home-manager modules here
+{
   imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    # You can also split up your configuration and import pieces of it here:
-    ./emacs
+    ../modules
   ];
 
   nixpkgs = {
@@ -18,19 +12,10 @@
         url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
         sha256 = "174zhagljwb54q8byv79nz3ykvaxa72bkw7i20r0w06xhklljlrs";
       }))
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
-    # Configure your nixpkgs instance
+    
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = (_: true);
     };
   };
@@ -40,10 +25,9 @@
     homeDirectory = "/home/tjaros";
   };
 
-  tjaros.emacs.enable = true;
+  modules.emacs.enable = true;
 
   home.packages = with pkgs; [ 
-    
     spotify
     lutris
     wine
@@ -58,13 +42,10 @@
     transmission-qt
   ];
 
-  # Enable home-manager and git
   programs.home-manager.enable = true;
   programs.git.enable = true;
 
-  # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
 }
