2e2500be7f1bb2d5b17c53f8374e759c4bcf62e2
Tomas Jaros <tjaros.822@gmail.com>
2023-08-10 17:42:50 +0200

    fix: disable global mode lsp

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -108,7 +108,6 @@ in {
 
           (add-to-list 'load-path "~/.emacs.d/lisp/lsp-bridge")
           (require 'lsp-bridge)
-          (global-lsp-bridge-mode)
           (add-hook 'direnv-envrc-mode-hook 'lsp-bridge-restart-process)
         '';
 
