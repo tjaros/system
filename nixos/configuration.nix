06cd9090c06ad93321ec0d6f7a0c8a439e893559
Tomas Jaros <tjaros.822@gmail.com>
2023-08-11 20:38:04 +0200

    fix: try fixing integrated gpu nvidia issue

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -43,6 +43,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams = [ "module_blacklist=i915" ];
 
   # Setup keyfile
   boot.initrd.secrets = {
