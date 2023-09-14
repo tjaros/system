276a21e4f251568d1bd6aced8a0d5c595e8d69e0
Tomas Jaros <tjaros.822@gmail.com>
2023-09-14 11:21:08 +0200

    feat: add tiling assistant extension

--- nixos/global/gnome.nix
+++ nixos/global/gnome.nix
@@ -4,6 +4,7 @@
   environment.systemPackages = with pkgs; [
     gnomeExtensions.appindicator
     kitty
+    gnomeExtensions.tiling-assistant
     gnome.gnome-tweaks
     gnome.zenity
     xorg.xhost
