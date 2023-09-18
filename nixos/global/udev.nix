2b4859d20d75b17f7a4e47265d53af07f78b249f
Tomas Jaros <tjaros.822@gmail.com>
2023-09-18 18:58:26 +0200

    feat: add chipwhisperer udev rules and group

--- nixos/global/udev.nix
+++ nixos/global/udev.nix
@@ -7,5 +7,12 @@
     # USB-Blaster II
     SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6010", MODE="0666"
     SUBSYSTEM=="usb", ATTR{idVendor}=="09fb", ATTR{idProduct}=="6810", MODE="0666"
+
+
+    # chipwhisperer
+
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2b3e", ATTRS{idProduct}=="*", MODE="0664", GROUP="chipwhisperer"
+    SUBSYSTEM=="tty", ATTRS{idVendor}=="2b3e", ATTRS{idProduct}=="*", MODE="0664", GROUP="chipwhisperer", SYMLINK+="cw_serial%n"
+    SUBSYSTEM=="tty", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="6124", MODE="0664", GROUP="chipwhisperer", SYMLINK+="cw_bootloader%n"     
     '';
 }
