dd6f634e6cd021995e6dcc287d841bb5a1cb712f
Tomas Jaros <tjaros.822@gmail.com>
2023-09-25 13:38:35 +0200

    feat add udev rule

--- nixos/global/udev.nix
+++ nixos/global/udev.nix
@@ -14,5 +14,7 @@
     SUBSYSTEMS=="usb", ATTRS{idVendor}=="2b3e", ATTRS{idProduct}=="*", MODE="0664", GROUP="chipwhisperer"
     SUBSYSTEM=="tty", ATTRS{idVendor}=="2b3e", ATTRS{idProduct}=="*", MODE="0664", GROUP="chipwhisperer", SYMLINK+="cw_serial%n"
     SUBSYSTEM=="tty", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="6124", MODE="0664", GROUP="chipwhisperer", SYMLINK+="cw_bootloader%n"     
-    '';
+
+    SUBSYSTEM=="tty", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="6124", MODE="0664", GROUP="plugdev", SYMLINK+="cw_bootloader%n"
+  '';
 }
