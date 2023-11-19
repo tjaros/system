6ec7882823d377f451418bf823b2998bb124dc27
Tomas Jaros <tjaros.822@gmail.com>
2023-11-19 17:54:08 +0100

    feat: lvim

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -4,21 +4,13 @@ with lib;
 
 let
   cfg = config.modules.neovim;
-  neovim-config = builtins.fetchGit {
-    url = "https://github.com/tjaros/neovim-config.git";
-    rev = "7521832d84f0d3875255afee4e7eefeab474d645";
-  };
 in {
-  options.modules.neovim.enable = mkEnableOption "neovim config without lazy nvim";
+  options.modules.neovim.enable = mkEnableOption "neovim config";
   
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      lazygit
-      bottom
-      gdu
+    home.packages = with pkgs.unstable; [
+      lunarvim
     ];
-
-    home.file.".config/nvim".source = neovim-config;
 
     programs.neovim = {
       enable = true;
