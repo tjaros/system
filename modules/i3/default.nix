3b142c0157a103a9647692142cd6f3f45a91ff0e
Tomas Jaros <tjaros.822@gmail.com>
2023-12-02 21:58:12 +0100

    feat: go for i3

--- modules/i3/default.nix
+++ modules/i3/default.nix
@@ -11,7 +11,7 @@ in
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       i3-rounded
-      material-icons
+      material-design-icons
       rxvt-unicode
       flameshot
       polybarFull
