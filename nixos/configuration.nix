abd117753615a824aa65c192fb1a41121fae841c
Tomas Jaros <tjaros.822@gmail.com>
2023-12-31 17:19:58 +0100

    feat: install xdg

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -42,6 +42,11 @@
     ];
 
   services.flatpak.enable = true;
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
   
   environment.systemPackages = with pkgs; [
     home-manager
