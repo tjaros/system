c79bde588ec4aba192126baddc275212aff93ae4
Tomas Jaros <tjaros.822@gmail.com>
2024-02-27 01:08:18 +0100

    fix: remove vbox

--- nixos/global/virtualization.nix
+++ nixos/global/virtualization.nix
@@ -3,8 +3,6 @@
 {
     virtualisation.libvirtd.enable = true;
     virtualisation.spiceUSBRedirection.enable = true;
-    virtualisation.virtualbox.host.enable = true;
-    virtualisation.virtualbox.host.enableExtensionPack = true;
 
     users.extraGroups.vboxusers.members = [ "tjaros" ];
 
