e387312c469790d12edbc8a3be25c9e8d0f40c0c
Tomas Jaros <tjaros.822@gmail.com>
2024-03-17 19:38:11 +0100

    feat: update emacs config

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -52,12 +52,6 @@ in {
 
           (menu-bar-mode -1)
 
-         (setq-default indent-tabs-mode nil)
-         (setq-default tab-width 4)
-         (setq indent-line-function 'insert-tab)
-         (setq c-default-style "linux") 
-         (setq c-basic-offset 4) 
-         (c-set-offset 'comment-intro 0) 
 
           (electric-pair-mode)
 
