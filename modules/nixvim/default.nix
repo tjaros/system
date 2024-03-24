550dc6c212bf0c1c2784931d4947a381c9265738
Tomas Jaros <tjaros.822@gmail.com>
2024-03-24 12:13:36 +0100

    feat: nvim-cmp

--- modules/nixvim/default.nix
+++ modules/nixvim/default.nix
@@ -18,6 +18,46 @@ in {
 
 	colorschemes.gruvbox.enable = true;
 
+	plugins.lsp = {
+	  enable = true;
+
+	  servers = {
+	    
+	  };
+	};
+
+	plugins.nvim-cmp = {
+	  enable = true;
+	  autoEnableSources = true;
+          sources = [
+            {name = "nvim_lsp";}
+            {name = "path";}
+            {name = "buffer";}
+            {name = "luasnip";}
+          ];
+
+          mapping = {
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<Tab>" = {
+              action = ''
+                function(fallback)
+                  if cmp.visible() then
+                    cmp.select_next_item()
+                  elseif luasnip.expandable() then
+                    luasnip.expand()
+                  elseif luasnip.expand_or_jumpable() then
+                    luasnip.expand_or_jump()
+                  elseif check_backspace() then
+                    fallback()
+                  else
+                    fallback()
+                  end
+                end
+              '';
+              modes = [ "i" "s" ];
+            };
+          };
+	};
       };
     };
 }
