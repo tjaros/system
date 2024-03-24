bcbf7aa3a978e5a9eafc38c8cc8bbd09eb6ada59
Tomas Jaros <tjaros.822@gmail.com>
2024-03-24 14:26:27 +0100

    feat: install telescope basic

--- modules/nixvim/plugins/telescope.nix
+++ modules/nixvim/plugins/telescope.nix
@@ -1,17 +1,41 @@
 {
   programs.nixvim.plugins.telescope = {
     enable = true;
-    extensions.file_browser.enable = true;
+    extensions = {
+      file_browser.enable = true;
+      fzf-native.enable = true;
+    };
+
+    defaults = {
+      layout_config = {
+        horizontal = {
+	  prompt_position = "top";
+	};
+      };
+      sorting_strategy = "ascending";
+    };
 
     keymaps = {
-      "<leader>f" = {
-        action = "find_files, {}"
-	desc = "Find project files"
-      };
       "<leader>/" = {
-        action = "live_grep";
-	desc = "Grep (root dir)"
-      }
+        action = "current_buffer_fuzzy_find, {}";
+        desc = "Buffer fzf.";
+      };
+      "<leader>:" = {
+        action = "command_history, {}";
+        desc = "Command History";
+      };
+      "<leader>ff" = {
+        action = "find_files, {}";
+        desc = "Find project files";
+      };
+      "<leader>fr" = {
+        action = "live_grep, {}";
+        desc = "Find text";
+      };
+      "<leader>fg" = {
+        action = "oldfiles, {}";
+        desc = "Recent";
+      };
     };
   };
 }
