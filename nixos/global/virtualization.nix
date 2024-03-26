85181de021dc7a5b53a14a40955cc57b50d4a8bc
Tomas Jaros <tjaros.822@gmail.com>
2024-03-26 10:49:34 +0100

    feat: install podman

--- nixos/global/virtualization.nix
+++ nixos/global/virtualization.nix
@@ -2,6 +2,18 @@
   virtualisation.libvirtd.enable = true;
   virtualisation.spiceUSBRedirection.enable = true;
 
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
   users.extraGroups.vboxusers.members = ["tjaros"];
 
   programs.dconf.enable = true;
